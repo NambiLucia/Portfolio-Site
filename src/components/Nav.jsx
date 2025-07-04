5ef38dab047ca72be6078d3f9f048b4a99935473
unknown <nambilucia@gmail.com>
2025-07-04 23:59:49 +0300

    fix render error

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+       <nav className="bg-gray-800 text-white p-4">
+      <h1 className="text-xl font-bold">My Navigation</h1>
+    </nav>
+    </div>
+  )
+}
+
+export default Nav
